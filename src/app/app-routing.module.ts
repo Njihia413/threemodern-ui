7a1bb0820c18d5f9c88d42791fcf0fb5437b6f43
Maureen Njihia <maureennjihia468@gmail.com>
2023-07-17 11:18:53 +0300

    Create login, register and reset password pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: 'reset', component: ResetPasswordComponent},
+  { path: '', redirectTo:"/login", pathMatch:"full"},
+];
+
+// const routes: Routes = [
+//   { path: 'main', component: MainpageComponent },
+//   { path: 'user-search', component: UserSearchComponent},
+//   { path: 'repository-search', component: RepositorySearchComponent},
+//   { path: '', redirectTo:"/main", pathMatch:"full"},
+//   { path: '**', component: NotFoundComponent},
+
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
